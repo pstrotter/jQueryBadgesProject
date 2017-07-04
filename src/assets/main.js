f2608b87d1b3199b3589142931967fcea0064d68
pstrotter <ptrotternz@gmail.com>
2017-07-04 15:48:24 +1200

    Completed steps

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
-  // your code will go here
 
+  $.ajax({
+      url: 'https://www.codeschool.com/users/ptrotter.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        addCourses(response.courses.completed);
+      }
+    });
+
+    function addCourses(courses) {
+
+      courses.forEach(function(course) {
+
+        var $badges = $('#badges');
+
+        var $course = $('<div />', {
+          'class'  : 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+
+      })
+    }
 });
